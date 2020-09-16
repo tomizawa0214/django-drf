b34bab046042b1f9c85c3c7f8b9828bc1d42896f
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-09-16 12:53:40 +0900

    APIデータ作成

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,22 @@
 <template>
   <v-app>
-    <router-view />
+    <v-app-bar color="success" dark app>
+      <v-toolbar-title>求人情報</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text to="/">ホーム</v-btn>
+        <v-btn text to="/editor">仕事追加</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-content>
+      <router-view />
+    </v-content>
+    <v-footer color="success" dark app class="justify-center">
+      &copy; 2020 TOMIZAWA Jun
+    </v-footer>
   </v-app>
 </template>
+
+<script>
+  export default {};
+</script>
