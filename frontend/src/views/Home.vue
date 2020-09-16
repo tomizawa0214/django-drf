4181c08c83f9f6eb667cca103eab0bd7cad4198b
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-09-16 11:31:59 +0900

    APIデータ取得

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <v-container>
+      <h1>Job Board</h1>
+      <div v-for="job in jobs" :key="job.pk">
+        <p class="mb-1">{{ job.company_name }}：{{ job.job_title }}</p>
+      </div>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  import { apiService } from "../common/api.service.js"
+  export default {
+    name: "home",
+    data() {
+      return {
+        jobs: []
+      }
+    },
+    methods: {
+      getJobs() {
+        let endpoint = "api/jobs/"
+        apiService(endpoint).then(data => {
+          this.jobs.push(...data.results)
+        })
+      }
+    },
+    created() {
+      this.getJobs()
+      console.log(this.jobs)
+    }
   }
-};
 </script>
